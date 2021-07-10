b519105c27a4c6eccd3c7bca7d6383471b371e3d
chlbri <51156116+chlbri@users.noreply.github.com>
2021-07-10 13:45:17 +0000

    Update hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,5 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
+export default (_: NextApiRequest, res: NextApiResponse): JSX.Element => {
   res.status(200).json({text: 'Hello'});
 };
