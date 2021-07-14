78e9c12f40d62cbb5f6fc2f0eb1c897be5a9b803
chlbri <51156116+chlbri@users.noreply.github.com>
2021-07-14 16:51:07 +0000

    Create _documents.tsx

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ReactElement, JSXElementConstructor, ReactFragment } from 'react';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<{
+    html: string;
+    head?: JSX.Element[];
+    styles?:
+      | ReactElement<any, string | JSXElementConstructor<any>>[]
+      | ReactFragment;
+  }> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {...initialProps};
+  }
+
+  render():JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
