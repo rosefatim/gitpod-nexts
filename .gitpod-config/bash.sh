5191cf9b2f18ecb4865aeefcc8d1a7d424cb42ef
chlbri <51156116+chlbri@users.noreply.github.com>
2021-08-10 14:06:20 +0000

    Update bash.sh

--- .gitpod-config/bash.sh
+++ .gitpod-config/bash.sh
@@ -1,6 +1,8 @@
 npm install -g ts-node
-yarn add -D typescript prettier jest-cucumber eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-next shelljs edit-json-file
-yarn add nanoid react react-dom next
-yarn add -D @types/react @types/node postcss@latest tailwindcss@latest autoprefixer@latest
+yarn add -D jest @types/jest ts-jest @types/jsonwebtoken jest-cucumber
+yarn add -D typescript prettier eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-next 
+yarn add nanoid jsonwebtoken react react-dom next cookie swr use-sse xstate @tippyjs/react dayjs
+yarn add -D shelljs edit-json-file
+yarn add -D @types/react @types/node @types/cookie postcss@latest tailwindcss@latest autoprefixer@latest
 npx tailwindcss init -p
 yarn
