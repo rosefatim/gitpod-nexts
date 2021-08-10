cacabfcd8691e812d4a3dc4002e70a7b81c9c8ad
chlbri <51156116+chlbri@users.noreply.github.com>
2021-08-10 15:30:21 +0000

    Update bash.sh

--- .gitpod-config/bash.sh
+++ .gitpod-config/bash.sh
@@ -1,5 +1,5 @@
 npm install -g ts-node
-yarn add -D jest @types/jest ts-jest @types/jsonwebtoken jest-cucumber
+yarn add -D jest @types/jest ts-jest react-test-renderer @types/react-test-renderer @types/jsonwebtoken jest-cucumber
 yarn add -D typescript prettier eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-next 
 yarn add nanoid jsonwebtoken react react-dom next cookie swr use-sse xstate @tippyjs/react dayjs
 yarn add -D shelljs edit-json-file
